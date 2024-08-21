b0cad79519994c9600c5ee7c54f1fa23df3d41cc
linconl-cmd <linconl.lee@gmail.com>
2024-08-21 08:08:35 -0300

    uploaded files

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('startGameBtn').addEventListener('click', function() {
+    window.location.href = 'https://html-classic.itch.zone/html/7304448/Gigi%20e%20o%20caminho%20da%20agua/index.html';
+});
